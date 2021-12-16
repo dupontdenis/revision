85745f86f8e2907ec9bb475f560b9c8050e7bb6e
Denis Dupont <denis.dupont@univ-evry.fr>
2021-12-16 08:54:30 +0100

    filter with parameter

---
+++ filter.js
@@ -0,0 +1,52 @@
+const data = [
+    {
+        nom: "coka",
+        cat: "Boisson",
+        prix: 0.4,
+    },
+    {
+        nom: "carotte",
+        cat: "Legume",
+        prix: 9,
+    },
+    {
+        nom: "farine",
+        cat: "Epicerie",
+        prix: 4,
+    },
+    {
+        nom: "cafe",
+        cat: "Petit-dej",
+        prix: 4,
+    },
+    {
+        nom: "jus de fruit",
+        cat: "Boisson",
+        prix: 2,
+    },
+    {
+        nom: "choux",
+        cat: "Legume",
+        prix: 4.3,
+    },
+    {
+        nom: "olives",
+        cat: "Epicerie",
+        prix: 60,
+    },
+    {
+        nom: "veau",
+        cat: "Boucherie",
+        prix: 40,
+    },
+    {
+        nom: "poireau",
+        cat: "Legume",
+        prix: 2,
+    }
+];
+
+const myCat = { cat : "Legume" }
+
+const test = data.filter( function({ cat }) {
+  return (cat.toLowerCase() == this.cat.toLowerCase()) }, myCat)
